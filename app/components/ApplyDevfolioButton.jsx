1f3ce7a0e4f2928b48e54bf6d660364fa9795dd9
Rishabh Jain <jainrishabh2610@gmail.com>
2026-01-01 21:57:21 +0530

    updates

--- app/components/ApplyDevfolioButton.jsx
+++ app/components/ApplyDevfolioButton.jsx
@@ -4,7 +4,7 @@ export default function ApplyDevfolioButton() {
   return (
     <div
       className="apply-button"
-      data-hackathon-slug="hacksync-2" // IMPORTANT: Replace with your actual hackathon slug
+      data-hackathon-slug="hacksync-7" // IMPORTANT: Replace with your actual hackathon slug
       data-button-theme="dark-inverted" // This theme will fit your dark website better
       style={{ height: 44, width: 312 }}
     />
